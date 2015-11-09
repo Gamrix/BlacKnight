3641945669094021d3c44cd3be54f29154386112
John Clow <johnwclow@hotmail.com>
2015-11-09 09:36:03 -0800

    added euca registration script

---
+++ puppet-no-ha/scripts/register_euca.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+euca_conf --register-service -T user-api -H 128.111.55.40 -N squeak_services
+usr/sbin/euca_conf --egister-walrusbackend --partition walrus --host 128.111.55.40  --component squeak_walrus
+/usr/sbin/euca_conf --register-cluster --partition squeak_zone --host  128.111.55.40 --component squeak_cc
+/usr/sbin/euca_conf --register-sc --partition squeak_zone --host 128.111.55.40 --component squeak_sc
+/usr/sbin/euca_conf --register-nodes "128.111.55.40 128.111.55.33"
